54bff32303da3a3566eafa91e8b21c606a1a0abc
atishagrawal <amitsaru@220365>
2015-09-15 15:29:47 +0530

    Added FloatableEditTexts

--- IOPApp/gen/com/softedge/iopapp/R.java
+++ IOPApp/gen/com/softedge/iopapp/R.java
@@ -85,6 +85,8 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
     public static final class drawable {
         public static final int ic_indiaonphone_logo=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int ic_mobile_number=0x7f020002;
+        public static final int ic_password=0x7f020003;
     }
     public static final class id {
         public static final int action_settings=0x7f090007;
@@ -108,6 +110,8 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int hello_world=0x7f060001;
         public static final int india_on_phone_logo_heading=0x7f060008;
         public static final int login_bar_login_text=0x7f060009;
+        public static final int mobile_number_id_hint=0x7f06000a;
+        public static final int password_hint=0x7f06000b;
         /**  Login Activity 
          */
         public static final int text_top_left=0x7f060003;
